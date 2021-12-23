ef13681354c57faf9b7b2750f369c2648b913765
KaiqueYudji <yudjikaique@gmail.com>
2021-12-23 20:32:59 -0300

    ok

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 
 let x = true;
+let i =0;
 
 const port = new SerialPort("COM3", {
     baudRate: 9600,
@@ -13,49 +14,48 @@ const parser = new Readline("\n");
 
 
 function SerialPrint(arg) {
+    
     setTimeout(() => port.write(arg),1000);
+    
      MudarValor();
 }
 
 function MudarValor(){
-
+    
+   
     return setTimeout(() => {
 
         let comands = ['Maria', 'Serial','Teste'];
-        let increment = 0;
-        let oi = increment++;
-        console.log('opa'+oi)
         x = true;
-
-        function incrementar(){
-            console.log('oi')
-            return increment++;
-        }
+        
 
         while(x === true){
+            
             x = false;
-            console.log(comands[increment]);
-            console.log('início: '+increment);
+
+            if( i === comands.length){
+                break
+            }
             
-            setTimeout(SerialPrint, 4000, comands[increment]);
+            setTimeout(SerialPrint, 2000, comands[i]);
+            i++;
             
-           
-             if(comands[increment] ==='Teste'){
-              x = false;
-             }
+            
+         console.log(comands.length)
+             
         
-            
-            
         }
     
-    }, 5000);
+    }, 3000);
+
+    
 }
 
 ///Loop through
 port.pipe(parser);
 parser.on("data", (line) => console.log(line))
 
-setTimeout(SerialPrint, 4000, 'Teste');
+setTimeout(SerialPrint, 4000, 'Tes');
 
 
 /* 
